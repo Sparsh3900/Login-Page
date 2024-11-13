7df07d27d46f3efea7b7d1ce0f7fd9e3169a4cb9
Sparsh3900 <101488750+Sparsh3900@users.noreply.github.com>
2024-11-13 16:08:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector("#login");
+
+btn.addEventListener("mouseover",function(){
+    btn.style.opacity = '0.8';
+    
+});
+
+btn.addEventListener("mouseout",function(){
+    btn.style.opacity = '1';
+    
+});
